daf521736566f50476b1afd72c039a4a9a42e0f2
luchersou <195580638+luchersou@users.noreply.github.com>
2026-04-20 18:38:23 -0300

    chore: add prisma.config.ts, update schema and create prisma client for v7

--- src/prisma.js
+++ src/prisma.js
@@ -1,3 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+import { PrismaClient } from "../prisma/generated/prisma/index.js";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-export const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
